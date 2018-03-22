1e9467d180af66e2c1461e04da51e9b362e59de2
Garrett May <maygarrett@hotmail.com>
2018-03-21 23:25:14 -0400

    [Level Assets] - Implemented some tile prefabs, some object prefabs, midway through an open door script

---
+++ Adventure Platformer/Assets/Scripts/Gate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour {
+
+    [SerializeField] private GameObject[] _states;
+    private int _currentState = 0;
+
+    [SerializeField] private float _timeBetweenStates;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_currentState == 0)
+            {
+                for (_currentState = 0; _currentState <= _states.Length; _currentState++)
+                {
+                    StartCoroutine(ChangeState());
+                    Debug.Log("End Loop");
+                }
+            }
+        }
+		
+	}
+
+    IEnumerator ChangeState()
+    {
+        
+
+        _states[_currentState].SetActive(true);
+
+        if (_currentState - 1 >= 0)
+        {
+            _states[_currentState - 1].SetActive(false);
+        }
+        Debug.Log("Current State Increase");
+
+        yield return new WaitForSeconds(_timeBetweenStates);
+    }
+}
