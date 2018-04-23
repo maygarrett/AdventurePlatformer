059766f57f83c18c2cd11e10c40a4f98edbf06e0
Garrett May <maygarrett@hotmail.com>
2018-04-22 21:24:45 -0400

    [Grass Level] - added new scene, some physics materials

---
+++ Adventure Platformer/Assets/Scripts/BasicCameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCameraFollow : MonoBehaviour {
+
+    [SerializeField] private float _yValue;
+
+    private Vector3 _locationStorage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _locationStorage = transform.position;
+        transform.position = new Vector3(_locationStorage.x, _yValue, _locationStorage.z);
+	}
+}
---
+++ Adventure Platformer/Assets/Scripts/Door.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    [SerializeField] private SpriteRenderer _doorIconRenderer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            _doorIconRenderer.enabled = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ActivateDoor();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _doorIconRenderer.enabled = false;
+        }
+    }
+
+    private void ActivateDoor()
+    {
+        SceneManager.instance.LoadLevel(0);
+    }
+}
